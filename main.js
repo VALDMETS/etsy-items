4084fe6f45add305a2683a638d566295fa78a265
Ben Valdmets-Harris <benvaldmets@gmail.com>
2016-06-28 16:24:27 -0500

    styled and working

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 var answers = [];
 var textAnswers = [];
 var h = 0;
+var g = [];
+
 
 //Answer 1
 items.forEach(function(item){
   h = h + item.price;
 });
 answers[0] = Math.round(100*(h/items.length))/100;
-
+textAnswers[0] = 'The average price is $' + answers[0];
 
 
 //Answer 2
@@ -15,9 +17,10 @@ h = items.filter(function(item){
     return (item.price>14 && item.price<18);
 });
 answers[1] = h.map(function(item){
-  return item.title;
+  return item.title + '<br><br>';
 });
-
+answers[1] = answers[1].join('');
+textAnswers[1] = String(answers[1]);
 
 
 //Answer 3
@@ -25,7 +28,7 @@ h = items.filter(function(item){
   return (item.currency_code === 'GBP');
 });
 answers[2] = [h[0].title, h[0].price];
-
+textAnswers[2] = answers[2][0] + ' costs £' + answers[2][1];
 
 
 //Answer 4
@@ -33,16 +36,26 @@ h = items.filter(function(item){
   return (item.materials.indexOf('wood') != -1);
 });
 answers[3] = h.map(function(item){
-  return item.title;
+  return item.title + '<br><br>';
 });
-
+answers[3] = answers[3].join('');
+textAnswers[3] = String(answers[3]);
 
 
 //Answer 5
 answers[4] = items.filter(function(item){
   return (item.materials.length >= 8);
 });
-
+h = answers[4].map(function(item){
+  return (item.materials);
+});
+for (var i = 0; i < answers[4].length; i++) {
+  for (var j = 0; j < h[i].length; j++) {
+    h[i][j] = h[i][j] + '<br>';
+  }
+  g[i] = '<br>' + answers[4][i].title + ' has ' + h[i].length + ' materials<br><br>' + h[i].join('');
+}
+textAnswers[4] = g.join('');
 
 
 //Answer 6
@@ -50,5 +63,11 @@ h = items.filter(function(item){
   return (item.who_made === 'i_did');
 });
 answers[5] = h.length;
+textAnswers[5] = answers[5] + ' were made by their sellers';
 
-console.log(answers);
+
+//Output Results
+for (var i = 0; i < textAnswers.length; i++) {
+  var temp = '#ans' + (i+1);
+  document.querySelector(temp).innerHTML = textAnswers[i];
+}
